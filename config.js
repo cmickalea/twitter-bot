a9c8c7fb671eba8f001e748cfb6a98b49e8ad2fe
Mickalea Chambers <iamchambers4@gmail.com>
2020-04-20 17:11:40 -0400

    bot done

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'rXDGtZ9pNbcwenFoxZaJpE495',
+    consumer_secret: 'Y57YCdpmzstEw8jtPT3707WD1yd2zkHZhS24KdeGVwmAls34ZZ',
+    access_token_key: '2770614353-fCY5bR5aEGpnX58FCoM2LMxS31ldYLemfuzrLxN',
+    access_token_secret: 'SxpRFZ9Nk2Wgyke022bKRutnSGhtZlEYkO0qw3tDwZ9kJ'
+};
